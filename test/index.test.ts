25fe0562f4d6ae60f85ff84f6e397056ac136937
Adam Janiš <adam.janis@gmail.com>
2022-04-09 22:38:15 +0100

    testing with miniflare jest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,58 +1,7 @@
-import { spawn } from "child_process";
-import { fetch } from "undici";
-import type { ChildProcess } from "child_process";
-import type { Response } from "undici";
+import worker from "../src/index";
 
-const waitUntilReady = async (url: string): Promise<Response> => {
-  let response: Response | undefined = undefined;
-
-  while (response === undefined) {
-    await new Promise((resolvePromise) => setTimeout(resolvePromise, 100));
-
-    try {
-      response = await fetch(url);
-    } catch {}
-  }
-
-  return response as Response;
-};
-const isWindows = process.platform === "win32";
-
-let wranglerProcess: ChildProcess;
-
-beforeAll(async () => {
-  wranglerProcess = spawn(
-    "npx",
-    [
-      "wrangler",
-      "dev",
-      "--local",
-      "--port",
-      "9001",
-    ],
-    {
-      shell: isWindows,
-      stdio: "inherit",
-    }
-  );
+test("responds with hello world", async () => {
+  const req = new Request("http://localhost/");
+  const res = await worker.fetch(req);
+  expect(await res.text()).toBe("Hello World!");
 });
-
-afterAll(async () => {
-  await new Promise((resolve, reject) => {
-    wranglerProcess.once("exit", (code) => {
-      if (!code) {
-        resolve(code);
-      } else {
-        reject(code);
-      }
-    });
-    wranglerProcess.kill();
-  });
-});
-
-it("renders 'Hello World!'", async () => {
-  const response = await waitUntilReady("http://localhost:9001/");
-  const text = await response.text();
-  expect(text).toBe("Hello World!");
-});
-
